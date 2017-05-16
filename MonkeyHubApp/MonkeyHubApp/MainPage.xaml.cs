9aab3b80931d9d08cf779617387805e3df676d3c
Alan Vargas <alandvgarcia@gmail.com>
2017-05-16 16:32:21 -0300

    Add project files.

---
+++ MonkeyHubApp/MonkeyHubApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MonkeyHubApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MonkeyHubApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+      
+    }
+}
---
+++ MonkeyHubApp/MonkeyHubApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using MonkeyHubApp.Model;
+
+namespace MonkeyHubApp.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        private const string BaseUrl = "https://monkey-hub-api.azurewebsites.net/api/";
+
+        public async Task<List<Tag>> GetTagsAsync()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await httpClient.GetAsync($"{BaseUrl}Tags").ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                {
+                    return JsonConvert.DeserializeObject<List<Tag>>(
+                        await new StreamReader(responseStream)
+                            .ReadToEndAsync().ConfigureAwait(false));
+                }
+            }
+
+            return null;
+        }
+
+
+
+        private String _searchTerm;
+        public String SearchTerm
+        {
+            get { return _searchTerm; }
+            set
+            {
+                if (SetProperty(ref _searchTerm, value))
+                    SearchCommand.ChangeCanExecute();
+            }
+        }
+
+
+        public ObservableCollection<Tag> Resultados { get; }
+
+        public Command SearchCommand { get; }
+
+        public MainViewModel()
+        {
+            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
+            Resultados = new ObservableCollection<Tag>();
+        }
+
+        private bool CanExecuteSearchCommand()
+        {
+            return string.IsNullOrWhiteSpace(SearchTerm) == false;
+        }
+
+        async private void ExecuteSearchCommand()
+        {
+            await Task.Delay(1000);
+
+            bool resposta = await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Você pesquisou por {SearchTerm} ?", "Sim", "Não");
+
+            if (resposta)
+            {
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Obrigado", "OK");
+                var resultado = await GetTagsAsync();
+
+                Resultados.Clear();
+
+                if (resultado != null)
+                {
+                    foreach (Tag r in resultado)
+                        Resultados.Add(r);
+                }
+
+
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Tente Novamente", "OK");
+                Resultados.Clear();
+            }
+        }
+    }
+}
